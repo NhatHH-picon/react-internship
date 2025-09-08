a9246de69bbf08bed1b6318c58ec47246a66994f
thanhmp-source <thanh.mp@picontechnology.com>
2025-09-08 15:13:23 +0700

    First commit

---
+++ src/Card.tsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="card-header">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <div className="card-header">
+            <h2 className="title">10 AI Frameworks and Libraries Every Developer Should Learn in 2025</h2>
+        </div>
+      </div>
+      </div>
+)
+}
+
+export default Card
